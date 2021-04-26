231750e8d554294fe4db27db59c8766dfec0fccd
Mathias Tavernier-Roux <mathias.tavernier-roux@laplateforme.io>
2021-04-26 02:21:33 +0200

    Création d'un page one page par défaut
    Ajout d'une page one page plus accueillante

--- app/Views/accueil.php
+++ app/Views/accueil.php
@@ -1,6 +1,50 @@
-Cette Page Devrait Contenir Votre Page D'Accueil One Page (En Une Seul Page)
+<!DOCTYPE html>
+<html>
 
-Ce Script PHP Redirige Vers L'Authenfication Dans Ce Cas Puisque la Page n'existe pas
-<?php
-redirect()->to('/Users/login');
-?>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title><?= $titre?></title>
+    <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css'); ?>">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="<?php echo base_url('assets/fonts/fontawesome-all.min.css'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/fonts/font-awesome.min.css'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/fonts/fontawesome5-overrides.min.css'); ?>">
+</head>
+
+<body class="bg-gradient-primary">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-9 col-lg-12 col-xl-10">
+                <div class="card shadow-lg o-hidden border-0 my-5">
+                    <div class="card-body p-0">
+                        <div class="row">
+                            <div class="col-lg-6 col-xl-12">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h4 class="text-dark mb-4">Projet ENP<br>Page D'Accueil</h4>
+                                    </div>
+                                    <form class="user">
+                                        <div class="mb-3"></div>
+                                        <h5 style="text-align: center;">Quel est Cette Page</h5>
+                                        <p class="text-center">Cette Page est une Page Web One Page prédéfini par le Créateur du Projet ENP</p>
+                                        <hr>
+                                        <h5 style="text-align: center;">A Quoi Sert Cette Page</h5>
+                                        <p class="text-center"><strong>A Rien</strong> <br>(Elle Est Censé Etre Remplacé par Une Page Web One Page Symbolisant La Page d'Accueil De Votre Entreprise)<br><br>Si Cette Page Apparait C'est que Votre Administrateur ENP Ne L'a pas Modifié</p><button class="btn btn-primary d-block btn-user w-100" type="link" href="/Users/login">Accéder a la page d'identification</button>
+                                        <div class="mb-3">
+                                            <div class="custom-control custom-checkbox small"></div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js'); ?>"></script>
+    <script src="<?php echo base_url('assets/js/theme.js'); ?>"></script>
+</body>
+
+</html>
