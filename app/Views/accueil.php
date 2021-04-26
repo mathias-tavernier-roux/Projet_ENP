4af562f94810a406838411d460624e21c8a716ca
Mathias Tavernier-Roux <mathias.tavernier-roux@laplateforme.io>
2021-04-26 02:00:54 +0200

    Modification de La Page Par Défaut Pour Rediriger

--- app/Views/accueil.php
+++ app/Views/accueil.php
@@ -1,1 +1,6 @@
 Cette Page Devrait Contenir Votre Page D'Accueil One Page (En Une Seul Page)
+
+Ce Script PHP Redirige Vers L'Authenfication Dans Ce Cas Puisque la Page n'existe pas
+<?php
+redirect()->to('/Users/login');
+?>
